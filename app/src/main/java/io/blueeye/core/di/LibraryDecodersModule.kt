300b7637015dd5d730c88642582c2dc93ced6e9d
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-07-07 17:18:44 +0200

    Trim decoder pipeline to Field MVP scope

--- app/src/main/java/io/blueeye/core/di/LibraryDecodersModule.kt
+++ app/src/main/java/io/blueeye/core/di/LibraryDecodersModule.kt
@@ -8,38 +8,26 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import io.blueeye.core.decoders.BleBeaconDecoder
-import io.blueeye.core.decoders.apple.*
 import io.blueeye.core.decoders.beacon.*
 import io.blueeye.core.decoders.bose.*
-import io.blueeye.core.decoders.environmental.*
-import io.blueeye.core.decoders.govee.*
-import io.blueeye.core.decoders.inkbird.*
 import io.blueeye.core.decoders.microsoft.*
-import io.blueeye.core.decoders.misc.*
+import io.blueeye.core.decoders.misc.GAENDecoder
+import io.blueeye.core.decoders.misc.ServiceDataDecoder
 import io.blueeye.core.decoders.misc.beacon.*
-import io.blueeye.core.decoders.misc.health.*
-import io.blueeye.core.decoders.misc.moko.*
-import io.blueeye.core.decoders.misc.sensor.*
+import io.blueeye.core.decoders.misc.moko.MokobeaconDecoder
 import io.blueeye.core.decoders.misc.tracker.*
-import io.blueeye.core.decoders.qingping.*
 import io.blueeye.core.decoders.samsung.*
-import io.blueeye.core.decoders.shelly.*
-import io.blueeye.core.decoders.switchbot.*
-import io.blueeye.core.decoders.xiaomi.*
 
 @Module
 @InstallIn(SingletonComponent::class)
 @Suppress("TooManyFunctions")
 abstract class LibraryDecodersModule {
-    // === TRACKERS & BEACONS ===
+    // === FIELD MVP TRACKERS, BEACONS, AUDIO, AND SIGNAL EVIDENCE ===
     @Binds @IntoSet
     abstract fun bindABN03Decoder(decoder: ABN03Decoder): BleBeaconDecoder
 
     @Binds @IntoSet
     abstract fun bindABN07Decoder(decoder: ABN07Decoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindAltBeaconDecoder(decoder: AltBeaconDecoder): BleBeaconDecoder
 
     @Binds @IntoSet
     abstract fun bindEddystoneDecoder(decoder: EddystoneDecoder): BleBeaconDecoder
@@ -71,28 +59,11 @@ abstract class LibraryDecodersModule {
     @Binds @IntoSet
     abstract fun bindHolyIotDecoder(decoder: HolyIotDecoder): BleBeaconDecoder
 
-    // === HEALTH/MEDICAL ===
-    @Binds @IntoSet
-    abstract fun bindABTempDecoder(decoder: ABTempDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindOralBDecoder(decoder: OralBDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindMiBandDecoder(decoder: MiBandDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindLaicaPS7200LDecoder(decoder: LaicaPS7200LDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindHeartRateDecoder(decoder: HeartRateDecoder): BleBeaconDecoder
-
-    // === MISC / IT / EXPERIMENTAL ===
     @Binds @IntoSet
     abstract fun bindMicrosoftCDPDecoder(decoder: MicrosoftCDPDecoder): BleBeaconDecoder
 
     @Binds @IntoSet
-    abstract fun bindGAENDecoder(decoder: GAENDecoder): BleBeaconDecoder // Exposure Notification (COVID)
+    abstract fun bindGAENDecoder(decoder: GAENDecoder): BleBeaconDecoder
 
     @Binds @IntoSet
     abstract fun bindGenericTrackerDecoder(decoder: GenericTrackerDecoder): BleBeaconDecoder
@@ -103,46 +74,6 @@ abstract class LibraryDecodersModule {
     @Binds @IntoSet
     abstract fun bindMikrotikDecoder(decoder: MikrotikDecoder): BleBeaconDecoder
 
-    // === OTHER SPECIFIC ===
-    @Binds @IntoSet
-    abstract fun bindAmphiroDecoder(decoder: AmphiroDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindBParasiteDecoder(decoder: BParasiteDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindBParasiteV2Decoder(decoder: BParasiteV2Decoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindTiltDecoder(decoder: TiltDecoder): BleBeaconDecoder // Homebrew hydrometer
-
-    @Binds @IntoSet
-    abstract fun bindSkaleDecoder(decoder: SkaleDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindSmartDryDecoder(decoder: SmartDryDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindBikeSharingDecoder(decoder: BikeSharingDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindAnemometerDecoder(decoder: AnemometerDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindRadiolandDecoder(decoder: RadiolandDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindINodeDecoder(decoder: INodeDecoder): BleBeaconDecoder
-
     @Binds @IntoSet
     abstract fun bindBoseDecoder(decoder: BoseDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindVchonDecoder(decoder: VchonDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindOrasDecoder(decoder: OrasDecoder): BleBeaconDecoder
-
-    @Binds @IntoSet
-    abstract fun bindHHCCRoPotDecoder(decoder: HHCCRoPotDecoder): BleBeaconDecoder
 }
