85ab344e54616f735ddfaac2696d3239cd666655
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-07-07 17:17:58 +0200

    Keep beacon decoders inside Field MVP scope

--- app/src/main/java/io/blueeye/core/di/BeaconDecodersModule.kt
+++ app/src/main/java/io/blueeye/core/di/BeaconDecodersModule.kt
@@ -10,8 +10,6 @@ import io.blueeye.core.decoders.AltBeaconDecoder
 import io.blueeye.core.decoders.AmazonAmaDecoder
 import io.blueeye.core.decoders.BleBeaconDecoder
 import io.blueeye.core.decoders.GoogleFastPairBeaconDecoder
-import io.blueeye.core.decoders.TP357SDecoder
-import io.blueeye.core.decoders.TheengsInteroperableDecoder
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -30,13 +28,5 @@ abstract class BeaconDecodersModule {
 
     @Binds
     @IntoSet
-    abstract fun bindTp357sDecoder(decoder: TP357SDecoder): BleBeaconDecoder
-
-    @Binds
-    @IntoSet
     abstract fun bindAirTagDecoder(decoder: AirTagBroadcastDecoder): BleBeaconDecoder
-
-    @Binds
-    @IntoSet
-    abstract fun bindTheengsDecoder(decoder: TheengsInteroperableDecoder): BleBeaconDecoder
 }
