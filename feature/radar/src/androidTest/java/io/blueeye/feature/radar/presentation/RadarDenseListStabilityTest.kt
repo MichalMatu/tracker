f554b0d5d97f5185cdd1bddba0411559ce2c8362
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-12 06:39:08 +0200

    Fix dense Radar instrumentation test API import

--- feature/radar/src/androidTest/java/io/blueeye/feature/radar/presentation/RadarDenseListStabilityTest.kt
+++ feature/radar/src/androidTest/java/io/blueeye/feature/radar/presentation/RadarDenseListStabilityTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.fetchSemanticsNode
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performScrollToIndex
